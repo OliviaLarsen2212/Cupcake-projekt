df8b6b1a79ba4de83e7af251315d8f2ed73cc64b
Hussain-Al-Kaseb <hussainfhl@hotmail.com>
2025-10-23 12:40:18 +0200

    accountmapper

---
+++ cupcake/src/main/java/app/entities/Basket.java
@@ -0,0 +1,43 @@
+package main.java.app.entities;
+import java.util.List;
+
+
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket {
+
+    private List<Orderline> orderlines = new ArrayList<>();
+
+    public void addOrderline(Orderline orderline) {
+
+        orderlines.add(orderline);
+    }
+
+    public List<Orderline> getOrderlines() {
+        return orderlines;
+    }
+
+    //This method is only used int the cart.html. That's why the methos is showen as not used
+    public int getOrderTotalPrice() {
+        int totalPrice = 0;
+
+        for (Orderline orderline : orderlines) {
+            totalPrice += orderline.getPricePrOrderLine();
+        }
+
+
+        return totalPrice;
+    }
+
+    public int getTotalAmountOfCupcakes() {
+        int amount = 0;
+
+        for (Orderline orderline : orderlines) {
+            amount += orderline.getAmount();
+        }
+        return amount;
+    }
+}
