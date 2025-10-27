a2a23295c8862803625f69b84cd9e2619cbe63cd
OliviaLarsen2212 <oliviasonder2212@gmail.com>
2025-10-27 12:16:28 +0100

    updated main

--- cupcake/src/main/java/app/Main.java
+++ cupcake/src/main/java/app/Main.java
@@ -1,6 +1,12 @@
 package app;
 
+import app.config.ThymeleafConfig;
+import app.controllers.PageController;
 import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class Main {
     private static final String USER = "postgres";
@@ -8,5 +14,29 @@ public class Main {
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "cupcake";
 
-    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+//    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args) {
+
+        // Opret Thymeleaf-engine
+        TemplateEngine engine = new TemplateEngine();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+        engine.setTemplateResolver(resolver);
+
+        // Start Javalin
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.hostedPath = "/";
+                staticFiles.directory = "/public";
+            });
+            config.fileRenderer(new JavalinThymeleaf(engine));
+        }).start(7070);
+
+        // Routes
+        PageController.addRoutes(app);
+    }
+
 }
